1fea23c05ca52c2dc244aa20d521ccf33896effe
veerababu <veeruwithrails@gmail.com>
2018-05-30 11:17:06 +0530

    Initial commit

---
+++ MainActivity.java
@@ -0,0 +1,163 @@
+package com.example.myvideorecorder;
+
+import android.Manifest;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.MediaMetadataRetriever;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.VideoView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_VIDEO_CAPTURE = 1;
+    VideoView mVideoView;
+    ImageView iv_disp;
+
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+    static {
+        System.loadLibrary("opencv_java3");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        mVideoView = findViewById(R.id.disp_vv);
+//        capturedImageView = findViewById(R.id.img_disp_iv);
+
+//        dispatchTakeVideoIntent();
+        iv_disp = findViewById(R.id.disp_iv);
+
+        ActivityCompat.requestPermissions(this,PERMISSIONS_STORAGE,1);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/raw/fight");
+        //Log.d("path = ",uri+"");
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        String video_duration="";
+        try {
+            retriever.setDataSource(this,uri);
+            video_duration = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
+            Log.d("Duration of video = ",video_duration);
+        }
+        catch (IllegalArgumentException iae)
+        {
+            Log.d("Status","NOPE");
+            iae.printStackTrace();
+        }
+        Bitmap bmpOriginal = retriever.getFrameAtTime(0);
+        int bmpVideoHeight = bmpOriginal.getHeight();
+        int bmpVideoWidth = bmpOriginal.getWidth();
+        byte [] lastSavedByteArray = new byte[0];
+
+        float factor = 20f;
+        int scaleWidth = (int) ( (float) bmpVideoWidth * factor );
+        int scaleHeight = (int) ( (float) bmpVideoHeight * factor );
+        int max = (int) Long.parseLong(video_duration);
+
+        for(int i=0;i<max;i++) {
+
+            bmpOriginal = retriever.getFrameAtTime(i*1000,MediaMetadataRetriever.OPTION_CLOSEST);
+            bmpVideoHeight = bmpOriginal.getHeight();
+            bmpVideoWidth = bmpOriginal.getWidth();
+            int byteCount = bmpVideoHeight*bmpVideoWidth*4;
+            ByteBuffer tempByteBuffer = ByteBuffer.allocate(byteCount);
+            bmpOriginal.copyPixelsToBuffer(tempByteBuffer);
+            byte[] tempByteArray = tempByteBuffer.array();
+
+            if(!Arrays.equals(tempByteArray,lastSavedByteArray)) {
+
+                File outputfile = new File("/sdcard/android/", "fight_"+i+".png");
+                OutputStream out = null;
+                try {
+                    out = new FileOutputStream(outputfile);
+                    Log.d("FILE", "Found");
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+                Bitmap bmpScaledSize = Bitmap.createScaledBitmap(bmpOriginal, scaleWidth, scaleHeight, false);
+                bmpScaledSize.compress(Bitmap.CompressFormat.PNG, 100, out);
+
+                try {
+                    assert out != null;
+                    Log.d("IMAGE STATUS", "Successful");
+                    out.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                lastSavedByteArray = tempByteArray;
+
+            }
+        }
+
+        iv_disp.setImageBitmap(bmpOriginal);
+        retriever.release();
+
+    }
+
+    private void dispatchTakeVideoIntent() {
+        Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
+            Uri videoUri = data.getData();
+            mVideoView.setVideoURI(videoUri);
+            mVideoView.start();
+            mVideoView.setOnClickListener(new View.OnClickListener() {
+                boolean paused = false;
+                @Override
+                public void onClick(View v) {
+                    if(v == mVideoView)
+                        Log.d("V==mVideoView","true");
+                    else
+                        Log.d("V==mVideoView","false");
+                    if (!paused) {
+                        mVideoView.pause();
+                        paused = true;
+                    }
+                    else {
+                        if (mVideoView.isPlaying())
+                            mVideoView.resume();
+                        else
+                            mVideoView.start();
+                        paused = false;
+                    }
+
+                }
+            });
+            Log.d("VIDEO PLAYING STATUS",""+mVideoView.isPlaying());
+        }
+    }
+}
