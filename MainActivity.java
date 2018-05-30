213ea231a50c25ee9b405c1995443787387d59e1
krishnasaijoga <krishnasaijoga@gmail.com>
2018-05-30 15:38:01 +0530

    "Extracted Frames from a video"

--- MainActivity.java
+++ MainActivity.java
@@ -145,7 +145,6 @@ public class MainActivity extends AppCompatActivity {
 //        SequenceEncoder
     }
 
-
     /*@Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 //        super.onActivityResult(requestCode, resultCode, data);
